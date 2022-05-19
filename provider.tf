f51b6a8cd082170d22d058ca528173cb4038e172
Kieran Wardle-Brown <kieran.wardle-brown@disneystreaming.com>
2022-05-19 23:36:47 +0100

    add default tags

--- provider.tf
+++ provider.tf
@@ -1,5 +1,16 @@
+locals {
+  default_tags = {
+    app        = "terraform-aws-baseline"
+    managed-by = "terraform"
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 # --------------------------------------------------------------------------------------------------
@@ -10,84 +21,152 @@ provider "aws" {
 provider "aws" {
   region = "ap-northeast-1"
   alias  = "ap-northeast-1"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "ap-northeast-2"
   alias  = "ap-northeast-2"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "ap-northeast-3"
   alias  = "ap-northeast-3"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "ap-south-1"
   alias  = "ap-south-1"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "ap-southeast-1"
   alias  = "ap-southeast-1"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "ap-southeast-2"
   alias  = "ap-southeast-2"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "ca-central-1"
   alias  = "ca-central-1"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "eu-central-1"
   alias  = "eu-central-1"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "eu-north-1"
   alias  = "eu-north-1"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "eu-west-1"
   alias  = "eu-west-1"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "eu-west-2"
   alias  = "eu-west-2"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "eu-west-3"
   alias  = "eu-west-3"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "sa-east-1"
   alias  = "sa-east-1"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "us-east-1"
   alias  = "us-east-1"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "us-east-2"
   alias  = "us-east-2"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "us-west-1"
   alias  = "us-west-1"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
   region = "us-west-2"
   alias  = "us-west-2"
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
